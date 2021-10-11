44086959040fa7e3b4f9ecf30aa1dbe5ddb9e9c1
ShamilFakhraziev <75545025+ShamilFakhraziev@users.noreply.github.com>
2021-10-12 00:54:35 +0500

    Адаптив

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 const schemeSvg = document.querySelector('.scheme-svg');
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector(".menu");
 let total = 0;
 const ticketPrice = 800;
 const totalPriceTag = document.querySelector('.price-total');
@@ -11,6 +13,14 @@ schemeSvg.addEventListener('click',(event)=>{
   }
   }
   
+});
+menuButton.addEventListener('click',(e)=>{
+  if(menu.style.display=="block"){
+    menu.style.display = 'none';
+  }
+  else{
+  menu.style.display = 'block';
+  }
 });
 
  
